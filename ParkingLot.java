16a35093404674d5cb3e351a10c1bbe04fee65c2
2450099 <69975432+2450099@users.noreply.github.com>
2021-01-07 11:44:40 -0600

    Works

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,1 +1,25 @@
 
+
+class ParkingLot {
+  private int numCars;
+  public final int MAX_CARS = 2500; 
+  public ParkingLot() {
+    numCars = 0;
+  }
+  public ParkingLot(int theNumCars) {
+    numCars = theNumCars;
+  }
+  public int getNumCars() {
+    return numCars;
+  }
+  public void setNumCars(int theNumCars) {
+    if (theNumCars >= 0 && theNumCars <= MAX_CARS) numCars = theNumCars;
+  }
+  public String toString() {
+    return "Number of cars is " + numCars;
+  }
+  public boolean equals(Object other) {
+    ParkingLot p = (ParkingLot)other;
+    return (numCars == (p.getNumCars()));
+  }
+}
